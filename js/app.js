6954004a2aab917af549354d6c514430eee5455c
Nicolas Coronel <nicolascoronel3888@gmail.com>
2020-08-21 20:16:08 -0300

    version 1.0

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#customers-testimonial").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 500,
+        loop: true,
+        autoPlayHoverPause: true
+    });
+});
